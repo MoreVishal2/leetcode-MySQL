be7b18b58597d84ca1d826241e1a9ec3de65333c
MoreVishal2 <144539062+MoreVishal2@users.noreply.github.com>
2026-03-03 17:06:59 +0530

    Time: 334 ms (83.5%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,4 +1,4 @@
-select q1.query_name as query_name , round(avg(rating/position),2) as quality, 
-round(ifnull(q2.bel3*100/count(q1.rating),0),2) as poor_query_percentage from Queries q1 left join
- (select query_name ,count(rating) bel3 from Queries where rating<3 group by query_name ) q2
-on q1.query_name=q2.query_name group by q1.query_name;
+SELECT query_name, ROUND(AVG(rating * 1.0 / position), 2) AS quality,
+ROUND( SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*),2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
