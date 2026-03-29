c1a3245c92e869e725fb2b76b5c128580ecb1ac3
MoreVishal2 <144539062+MoreVishal2@users.noreply.github.com>
2026-03-29 10:55:53 +0530

    Time: 1382 ms (98.95%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,5 @@
+select "High Salary" as category , count(*) as accounts_count from Accounts where income>50000
+union 
+select "Average Salary" ,count(*) from Accounts where income between 20000 and 50000
+union 
+select "Low Salary",count(*) from Accounts where income<20000;
